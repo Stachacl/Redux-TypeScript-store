832738c4f9cfb23a25cc7eb1e6711f296cc1a959
Stachacl <stacha.l@icloud.com>
2022-09-24 20:15:43 +1000

    cart number of items counter is working now

--- src/features/cart/CartLink.tsx
+++ src/features/cart/CartLink.tsx
@@ -5,10 +5,12 @@ import { getNumItems } from "./cartSlice";
 import { useAppSelector } from "../../app/hooks";
 
 export function CartLink() {
+    // if there are any imems - display them, othervise display the word cart
+
   const numItems = useAppSelector(getNumItems);
   return (
     <Link to="/cart" className={styles.link}>
       <span className={styles.text}>🛒&nbsp;&nbsp;{ numItems ? numItems : "Cart"}</span>
     </Link>
-  );
+        );
 }
